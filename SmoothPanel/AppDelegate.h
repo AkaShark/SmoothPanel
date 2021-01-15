a1f131b930559f5f5808750231a88fad0dcdcd03
Sharker <1548742234@qq.com>
2021-01-15 12:13:05 +0800

    Initial Commit

---
+++ SmoothPanel/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  SmoothPanel
+//
+//  Created by Sharker on 2021/1/15.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ SmoothPanel/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  SmoothPanel
+//
+//  Created by Sharker on 2021/1/15.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
